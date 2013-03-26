d3e8eb0773fcab489d29fe7be815b7efc9d4bbbb
shimovolos <shimovolos.aa.09@gmail.com>
2013-03-26 13:47:58 +0300

    remove config and changes for index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__).'/protected/globals.php');
 header('Content-Type: text/html; charset=UTF-8',true);
-$yii=dirname(__FILE__).'/../../../framework/yii.php';
+$yii=dirname(__FILE__).'/../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 require_once($yii);
